022994910b87f3496d13e9668c3b752b9e0301dc
volta2030 <volta2030@gmail.com>
2023-11-27 06:37:40 +0900

    -Add syncDevice(), getDeviceCount(), setDevice(), setDeviceFlags()

--- src/main/kotlin/KudaRuntimeAPI.kt
+++ src/main/kotlin/KudaRuntimeAPI.kt
@@ -1,6 +1,10 @@
 class KudaRuntimeAPI {
+    external fun syncDevice() : Int
     external fun getRuntimeVersion(): Int
     external fun getDivice() : Int
+    external fun getDeviceCount() : Int
+    external fun setDevice(device : Int) : Int
+    external fun setDeviceFlags(flags : Int) : Int
 
     companion object {
         init {
