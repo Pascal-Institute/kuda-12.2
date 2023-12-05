31c22b09a9d5c70753a46fca1733c89170005192
volta2030 <volta2030@gmail.com>
2023-12-05 21:18:59 +0900

    -Modify method names...

--- src/main/kotlin/kuda/RuntimeAPI.kt
+++ src/main/kotlin/kuda/RuntimeAPI.kt
@@ -6,22 +6,22 @@ import kuda.type.Limit
 
 class RuntimeAPI {
 
-    fun getLimit(limit : Limit) : Int {
-        return getLimit(limit.byte)
+    fun deviceGetLimit(limit : Limit) : Int {
+        return deviceGetLimit(limit.byte)
     }
-    private external fun getLimit(byte: Byte) : Int
-    external fun getPCIBusId(device : Int) : String
-    external fun getStreamPriorityRange() : Int
-    private external fun setCacheConfig(cacheConfig : Int) : Int
-    fun setCacheConfig(functionCache: FunctionCache) : Int{
-        return setCacheConfig(functionCache.num)
+    private external fun deviceGetLimit(byte: Byte) : Int
+    external fun deviceGetPCIBusId(device : Int) : String
+    external fun deviceGetStreamPriorityRange() : Int
+    private external fun deviceSetCacheConfig(cacheConfig : Int) : Int
+    fun deviceSetCacheConfig(functionCache: FunctionCache) : Int{
+        return deviceSetCacheConfig(functionCache.num)
     }
-    private external fun setLimit(limit : Byte, size : Int) : Int
-    fun setLimit(limit: Limit, size: Int) : Int{
-        return setLimit(limit.byte, size)
+    private external fun deviceSetLimit(limit : Byte, size : Int) : Int
+    fun deviceSetLimit(limit: Limit, size: Int) : Int{
+        return deviceSetLimit(limit.byte, size)
     }
-    external fun syncDevice() : Int
-    external fun getRuntimeVersion(): Int
+    external fun deviceSynchronize() : Int
+    external fun runtimeGetVersion(): Int
     external fun getDevice() : Int
     external fun getDeviceCount() : Int
     external fun setDevice(device : Int) : Int
--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import kuda.*
-import kuda.runtime.EventHandler
 import kuda.type.Error
 import kuda.type.FunctionCache
 import kuda.type.Limit
@@ -8,37 +7,32 @@ fun main(args: Array<String>) {
     val runtimeAPI = RuntimeAPI()
     val driverAPI = DriverAPI()
 
-    val cudaVersion = runtimeAPI.getRuntimeVersion()
+    val cudaVersion = runtimeAPI.runtimeGetVersion()
     val driverVersion = driverAPI.getDriverVersion()
 
     val device = runtimeAPI.getDevice()
     val driverDevice = driverAPI.getDevice(0)
     val deviceCount = driverAPI.getDeviceCount()
-
-    val eventHandler = EventHandler()
-
     driverAPI.init(0)
     println("CUDA Version : $cudaVersion")
     println("CUDA Driver version : $driverVersion")
     println("CUDA Device: $device")
     println("CUDA Device count : $deviceCount")
     println(driverDevice)
-    println(runtimeAPI.getLimit(Limit.PRINT_FIFO_SIZE))
-    println(runtimeAPI.getLimit(Limit.MALLOC_HEAP_SIZE))
-    println(runtimeAPI.getLimit(Limit.STACK_SIZE))
-    println(runtimeAPI.getLimit(Limit.DEV_RUNTIME_SYNC_DEPTH))
-    println(runtimeAPI.getLimit(Limit.MAX_L2_FETCH_GRANULARITY))
-    println(runtimeAPI.getLimit(Limit.DEV_RUNTIME_PENDING_LAUNCH_COUNT))
-    println(runtimeAPI.getLimit(Limit.PERSISTING_L2_CACHE_SIZE))
+    println(runtimeAPI.deviceGetLimit(Limit.PRINT_FIFO_SIZE))
+    println(runtimeAPI.deviceGetLimit(Limit.MALLOC_HEAP_SIZE))
+    println(runtimeAPI.deviceGetLimit(Limit.STACK_SIZE))
+    println(runtimeAPI.deviceGetLimit(Limit.DEV_RUNTIME_SYNC_DEPTH))
+    println(runtimeAPI.deviceGetLimit(Limit.MAX_L2_FETCH_GRANULARITY))
+    println(runtimeAPI.deviceGetLimit(Limit.DEV_RUNTIME_PENDING_LAUNCH_COUNT))
+    println(runtimeAPI.deviceGetLimit(Limit.PERSISTING_L2_CACHE_SIZE))
 
-    println(runtimeAPI.getPCIBusId(device))
-    println(runtimeAPI.getStreamPriorityRange())
+    println(runtimeAPI.deviceGetPCIBusId(device))
+    println(runtimeAPI.deviceGetStreamPriorityRange())
 
-    println(runtimeAPI.setCacheConfig(FunctionCache.PREFER_NONE))
+    println(runtimeAPI.deviceSetCacheConfig(FunctionCache.PREFER_NONE))
 
     println(runtimeAPI.getErrorName(Error.ERROR_UNKNOWN))
     println(runtimeAPI.getErrorString(Error.INITIALIZATION_ERROR))
-
-    println(eventHandler.create(123L))
 
 }
