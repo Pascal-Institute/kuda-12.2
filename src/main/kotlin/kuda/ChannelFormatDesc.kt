9fe967e3cec5b06db5baeedb862c87cf4f50ffb1
volta2030 <volta2030@gmail.com>
2023-12-29 00:19:25 +0900

    -Add ChannerFormatDesc

---
+++ src/main/kotlin/kuda/ChannelFormatDesc.kt
@@ -0,0 +1,13 @@
+package kuda
+
+//https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaChannelFormatDesc.html#structcudaChannelFormatDesc
+
+import kuda.prop.ChannelFormatKind
+
+data class ChannelFormatDesc(
+    val f: ChannelFormatKind,
+    val w: Int,
+    val x: Int,
+    val y: Int,
+    val z: Int
+)
