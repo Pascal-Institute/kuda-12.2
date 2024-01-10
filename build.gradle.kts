087f11d8e4be962fb8401e58620a8983aaa8e6a1
volta2030 <volta2030@gmail.com>
2024-01-11 07:02:14 +0900

    -Version Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
             groupId = "com.kudakit"
             artifactId = "kuda"
 
-            version = "1.0.9"
+            version = "1.0.10"
 
             from(components["java"])
         }
