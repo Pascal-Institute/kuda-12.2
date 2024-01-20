e1b3da9d39792395f487674c9b3e41b98ab60098
volta2030 <volta2030@gmail.com>
2024-01-20 12:50:55 +0900

    -Add getErrorName(), getErrorString()

--- src/main/kotlin/kuda/driverapi/DriverAPI.kt
+++ src/main/kotlin/kuda/driverapi/DriverAPI.kt
@@ -1,7 +1,17 @@
 package kuda.driverapi
 
+import kuda.driverapi.prop.Result
+
 class DriverAPI {
 
+    private external fun getErrorName(error : Int) : String
+    fun getErrorName(error : Result) : String {
+        return getErrorName(error.num)
+    }
+    private external fun getErrorString(error : Int) : String
+    fun getErrorString(error : Result) : String {
+        return getErrorString(error.num)
+    }
     external fun init(flags : Int) : Int
     external fun getDriverVersion() : Int
     external fun getDevice(ordinal : Int) : Int
