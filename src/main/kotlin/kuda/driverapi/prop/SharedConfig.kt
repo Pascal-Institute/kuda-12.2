ebcfd8202b9b59880c9bedf4f2c03f1b867c343e
volta2030 <volta2030@gmail.com>
2024-01-30 22:40:48 +0900

    -Add SharedConfig enum

---
+++ src/main/kotlin/kuda/driverapi/prop/SharedConfig.kt
@@ -0,0 +1,15 @@
+package kuda.driverapi.prop
+
+/**
+ *
+ * Shared memory configurations (CUsharedconfig)
+ *
+ * @property DEFAULT_BANK_SIZE set default shared memory bank size
+ * @property FOUR_BYTE_BANK_SIZE set shared memory bank width to four bytes
+ * @property EIGHT_BYTE_BANK_SIZE set shared memory bank width to eight bytes
+ */
+enum class SharedConfig(val num : Int) {
+   DEFAULT_BANK_SIZE(0x00),
+   FOUR_BYTE_BANK_SIZE(0x01),
+   EIGHT_BYTE_BANK_SIZE(0x02)
+}
